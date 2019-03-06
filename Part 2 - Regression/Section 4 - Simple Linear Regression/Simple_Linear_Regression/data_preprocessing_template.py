9e56b0a2b20b06fe78a000d5ea77587d8c83d8b2
Thomas Bigger <thomas@performanceactive.com>
2019-03-06 14:42:53 +0000

    plotting graphs for training set and test set with simple linear regression

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/data_preprocessing_template.py
@@ -22,10 +22,35 @@ regressor = LinearRegression()
 # fit regressor to the training set
 regressor.fit(X_train, y_train)
 
-# predicting with the test results
+# predicting with the test results, the new observations
+# y_test is the real salaries, y_pred is the predicted salaries
 y_pred = regressor.predict(X_test)
 
 
 # Visualising the training set results
+# plot our observation points
 plt.scatter(X_train, y_train, color='red')
 
+# plot the line
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualising the test set results
+
+# the line stays because it is a part of the regressor object
+
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# we can see how good the prediction is because of how many are close to the line
+
+# for new predictions we can input new X values and get predictions for salary
