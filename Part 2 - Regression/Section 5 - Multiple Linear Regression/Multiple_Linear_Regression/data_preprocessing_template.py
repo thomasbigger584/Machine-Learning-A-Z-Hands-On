81982c8f1654802a0990989844b65c2ca49f81f4
Thomas Bigger <thomas@performanceactive.com>
2019-03-07 12:28:21 +0000

    done multiple linear regression template

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/data_preprocessing_template.py
@@ -1,23 +1,39 @@
 # Data Preprocessing Template
 
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 4].values
+
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+#however this gives numbers for each of the categories which could imply one is greater than the other
+#change text into numbers
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+
+#we need to apply it to craete a column for each category, 0 or 1 for that category
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the dummy variable trap
+#remove the first column as it is one of the categories
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+# Fitting Multiple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set Results
+y_pred = regressor.predict(X_test)
