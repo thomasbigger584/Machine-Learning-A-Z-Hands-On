50c8761411ed03650983f9b24a2ae736b19958b3
Thomas Bigger <thomas@performanceactive.com>
2019-03-07 15:27:04 +0000

    backwards elimination methods being called with the dataset

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/backward_elimination_p_and_r.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/backward_elimination_p_and_r.py
@@ -1,6 +1,50 @@
-# -*- coding: utf-8 -*-
+# Data Preprocessing Template
 
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+#however this gives numbers for each of the categories which could imply one is greater than the other
+#change text into numbers
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+
+#we need to apply it to craete a column for each category, 0 or 1 for that category
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the dummy variable trap
+#remove the first column as it is one of the categories, we want to remove one of the categories  columns as its inferred
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting Multiple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set Results
+y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
 import statsmodels.formula.api as sm
+
+# add a column of 1's at the start so as it goes with the linear regression formula b0 constant
+# this is done in preparation to backwards elimination
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
 def backwardElimination(x, SL):
     numVars = len(x[0])
     temp = np.zeros((50,6)).astype(int)
