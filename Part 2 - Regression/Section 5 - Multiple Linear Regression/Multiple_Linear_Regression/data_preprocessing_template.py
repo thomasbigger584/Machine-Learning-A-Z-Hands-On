3ebd3f9fb80eca12ff4c2a6104fbbcab14b0d8c0
Thomas Bigger <thomas@performanceactive.com>
2019-03-07 15:04:28 +0000

    backwards elimination steps taken to make a better model

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/data_preprocessing_template.py
@@ -23,7 +23,7 @@ onehotencoder = OneHotEncoder(categorical_features=[3])
 X = onehotencoder.fit_transform(X).toarray()
 
 #Avoiding the dummy variable trap
-#remove the first column as it is one of the categories
+#remove the first column as it is one of the categories, we want to remove one of the categories  columns as its inferred
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
@@ -37,3 +37,65 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set Results
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+
+# add a column of 1's at the start so as it goes with the linear regression formula b0 constant
+# this is done in preparation to backwards elimination
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+# will contain only variables which have high significance later
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+
+# STEP 2: fit the full model with all possible predictors
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+# STEP 3: consider the predictor with the highest p-value and remove if P > SL
+# contains useful metrics to make model more robust. has the P values.
+# the lower the p value the more significant the independant variable is going to be WRT the dependant variables
+# const = x0 = 1 we added previous
+# x1, x2 = 2 categorical dummy variables for state
+# x3 = R&D Spend
+# x4 = Admin Spend
+# x5 = Marketing Spend
+
+# look for the highest p value, it is x2 with 0.990, most above SL = 0.05, we need to remove x2
+print(regressor_OLS.summary())
+
+
+# STEP 4 removing index 2, as it is x2 with the highest p value
+X_opt = X[:, [0, 1, 3, 4, 5]]
+
+# STEP 5: fit the full model with all remaining predictors
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
+
+# Iterate this until all P < SL
+
+# ----------------------------------------------
+
+# STEP 4 removing index 1, x1
+X_opt = X[:, [0, 3, 4, 5]]
+
+# STEP 5: fit the full model with all remaining predictors
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
+
+
+# In the case of P alue = 0.000, the P value is so small it is rounded to 0.00
+
+# STEP 4 removing index 2, x2
+X_opt = X[:, [0, 3, 5]]
+
+# STEP 5: fit the full model with all remaining predictors
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
+
+
+# STEP 4 removing index 2, x2
+X_opt = X[:, [0, 3]]
+
+# STEP 5: fit the full model with all remaining predictors
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+print(regressor_OLS.summary())
