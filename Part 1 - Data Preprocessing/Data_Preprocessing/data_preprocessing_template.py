eb4d61dd832f889ff69c3d562055b15f8c536977
Thomas Bigger <thomas@performanceactive.com>
2019-03-14 12:29:31 +0000

    done template for svr

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -15,7 +15,7 @@ X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 
 from sklearn.preprocessing import StandardScaler
--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/regression_template.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/regression_template.py
@@ -15,20 +15,36 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+# scale matrix for features X and dependent variables y
+# two separate standardscaler objects because of two separate matrices
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
+# Fitting the SVR Model to the dataset
+
+from sklearn.svm import SVR
+
+# C: play to regular 
+# KERNEL: what type of svr do you want, linear, polynomial, rbf
+# rbf most common, also default but add anyway
+
+# this SVR class does not apply feature scaling
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+# because we are using feature scaling, we want to scale 6.5 into something the regressor knows to predict
+transformed_test_X = sc_X.transform(np.array([[6.5]]))
+#this is the scalled prediction
+y_pred = regressor.predict(transformed_test_X)
+#we want to reverse to get the actual prediction
+y_pred = sc_y.inverse_transform(y_pred)
 
-# Visualising the Regression results
+
+# Visualising the SVR results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Regression Model)')
@@ -36,12 +52,12 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Regression results (for higher resolution and smoother curve)
+# Visualising the SVR results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
